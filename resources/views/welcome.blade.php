7b7c6919f2478dbf1c1c61ee492472a1847972a3
Empire Tech House <empiretechhouse@gmail.com>
2021-06-29 21:30:40 +0300

    Changed system title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Top Notch Homeworks')])
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('The Exchange')])
 
 @section('content')
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
-          <h1 class="text-white text-center">{{ __('Welcome to Top Notch Homeworks.') }}</h1>
+          <h1 class="text-white text-center">{{ __('Welcome to The Exchange.') }}</h1>
       </div>
   </div>
 </div>
